9fb6b23abffe15d9c7424810b142d41dd99bad12
fifa_ <70844552+fiifa@users.noreply.github.com>
2022-04-04 16:00:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
+const express=require("express"),app=express();app.get("/",function(e,p){p.send("bot is online")}),app.listen(3e3);
 
+
+
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: 32767 });
+
+client.on('ready', () => {
+  
+ 
+  
+  client.user.setActivity('fifaaaa', { type: 'STREAMING' })
+}) 
+
+   
+     
+  console.log('ONLINE');
+
+
+  
+  const modra = '#000AFF'
+ 
+
+const fs = require("fs");
+const prefix = ",";
+client.commands = new Discord.Collection();
+const commands = fs.readdirSync("./commands").filter(file => file.endsWith(".js"))
+for(file of commands) {
+  const commandName = file.split(".")[0]
+  const command = require(`./commands/${commandName}`)
+  client.commands.set(commandName, command)
+}
+
+ 
+
+
+  client.on('messageCreate', message => {
+    
+
+        
+     
+		
+    
+    
+    if (message.content.startsWith(prefix)) {
+ const args = message.content.slice(prefix.length).trim().split(/ +/g)
+      if (message.content.startsWith("!embed")) {
+ message.channel.send(exampleEmbed)
+       } 
+const commandName = args.shift()
+const command = client.commands.find((x, y) => y.toLowerCase() === commandName.toLowerCase() || (x.aliases && x.aliases.find(z => z.toLowerCase() === commandName.toLowerCase()))); 
+    if(!command) return //message.reply({content: "Tento příkaz neexistuje."})
+    command.run(client, message, args)
+  }    
+
+            });
+             
+    
+
+
+
+
+
+
+
+
+  
+
+
+client.login(process.env.token);
