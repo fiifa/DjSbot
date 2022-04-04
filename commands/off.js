b0fa761515610f1bad1194175c23d6c09b8a66a9
fifa_ <70844552+fiifa@users.noreply.github.com>
2022-04-04 16:12:22 +0200

    Create off.js

---
+++ commands/off.js
@@ -0,0 +1,12 @@
+module.exports.run = (client, message, args) => {
+
+  const Discord = require('discord.js')
+
+
+
+if (message.author.id !== "YOUR-ID") return;
+message.reply("🔔 | Bye...")
+process.exit();
+  
+}
+
