722e6922c5496613d0daae59ddf97f8d38901bc0
fifa_ <70844552+fiifa@users.noreply.github.com>
2022-04-04 16:03:38 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,1 +1,31 @@
+module.exports.run = async(client, message, args) => {
+  
+const Discord = require('discord.js')
+  
+  
+  if(!message.member.permissions.has("BAN_MEMBERS"))
+return message.channel.send(`Nemáš dostatečná oprávnění k provedení tohoto příkazu.`)
+
+const member = message.mentions.members.last() || message.guild.members.cache.get(args[0]);
+if(!member) return message.channel.send(`Zmiň člověka, kterého chceš zabanovat.`)
+
+var reason = args[1]
+if(!reason) reason = 'Nebyl uveden žádný důvod banu.'
+
+if(member.user.id === message.author.id) return message.channel.send(`Proč bys chtěl dát ban sám sobě? 😦`)
+
+if(member.user.id === '841303290359578665') return message.channel.send(`Promiň, ale jeho zabanovat nedokážu. #Panda 😦`)
+
+if(member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send(`Proč se pokoušíš zabanovat větší roli, než jsi ty? To, že jsi pod ním má jistý důvod když se ho snažíš zabanovat.`)
+
+var msg = await message.channel.send(`**Úspěšně jsem zabanoval:** ${member.user.tag} **z důvodu:** `+args.slice(1).join(" "))
+
+message.guild.members.ban(member, {reason: message.author.tag+" - "+args.slice(1).join(" ")}).catch(err => {
+return msg.edit(`Nemám dostatečná oprávnění na provedení tohoto příkazu.`).catch(err => console.log(err))
+})
+  } 
+
+
+
+
 
