ca18608fb82027c7a47ee91124afac0c906fcaf4
fifa_ <70844552+fiifa@users.noreply.github.com>
2022-05-17 18:36:27 +0200

    Create messageCreate.js

---
+++ SlashCommands/events/messageCreate.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const x = "😱";
+                        
+module.exports = async(client, message) => {
+  
+  if(message.content.toLowerCase() === "fifa") {
+message.reply("Epic secret! Fifa was here. 17/05/2022")
+
+
+}
+    }
